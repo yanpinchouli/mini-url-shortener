b9b6c6ab94dcb687ede9f4e93f94a4161a76575e
Yanpin Chouli <a131234568@gmail.com>
2026-03-10 22:28:17 +0800

    style(index.ts): format code by eslint/prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express'
 
-const app = express();
-const port = 8080;
+const app = express()
+const port = 8080
 
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
-  console.log(`listening on port http://localhost:${port}`);
-});
+  console.log(`listening on port http://localhost:${port}`)
+})
