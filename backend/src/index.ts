704519b85bf2a56aeb4740cb97ab5e7a658425a2
Yanpin Chouli <a131234568@gmail.com>
2026-03-15 15:20:56 +0800

    feat(auth): add express-session and redis

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,41 @@
-import express, { Request, Response } from 'express'
+import { RedisStore } from 'connect-redis'
+import 'dotenv/config'
+import express, { Express, Router } from 'express'
+import session from 'express-session'
+import { createClient, RedisClientType } from 'redis'
 
-const app = express()
-const port = 8080
+const app: Express = express()
+const port: number = Number(process.env.PORT)
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello World!')
+const redisClient: RedisClientType = createClient({
+  url: process.env.REDIS_URL,
 })
+await redisClient.connect()
+
+if (!process.env.SESSION_SECRET) {
+  throw new Error('SESSION_SECRET is not defined')
+}
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+    },
+  })
+)
+
+const router: Router = express.Router()
+
+app.use('/api/v1', router)
 
 app.listen(port, () => {
-  console.log(`listening on port http://localhost:${port}`)
+  console.log(`Server running at http://127.0.0.1:${port}`)
 })
